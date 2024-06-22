97773957b904fffe32a9616815e22909928d5f9d
Alexey <alexey.anasov@yandex.by>
2024-06-22 14:34:20 +0300

    Выделение ошибки и изменение value в inputCost

--- js/view/costInput.js
+++ js/view/costInput.js
@@ -1,4 +1,5 @@
 function init(getData) {
+  const data = getData();
   const input = document.querySelector("#input-cost");
 
   const settings = {
@@ -7,7 +8,40 @@ function init(getData) {
     delimiter: " ",
   };
 
-  new Cleave(input, settings);
+  const cleaveInput = new Cleave(input, settings);
+  cleaveInput.setRawValue(data.cost);
+
+  input.addEventListener("input", function () {
+    const value = +cleaveInput.getRawValue();
+
+    if (value < data.minPrice || value > data.maxPrice) {
+      input.closest(".param__details").classList.add("param__details--error");
+    }
+
+    if (value >= data.minPrice && value <= data.maxPrice) {
+      input
+        .closest(".param__details")
+        .classList.remove("param__details--error");
+    }
+  });
+
+  input.addEventListener("change", function () {
+    const value = +cleaveInput.getRawValue();
+
+    if (value > data.maxPrice) {
+      input
+        .closest(".param__details")
+        .classList.remove("param__details--error");
+      cleaveInput.setRawValue(data.maxPrice);
+    }
+
+    if (value < data.minPrice) {
+      input
+        .closest(".param__details")
+        .classList.remove("param__details--error");
+      cleaveInput.setRawValue(data.minPrice);
+    }
+  });
 }
 
 export default init;
