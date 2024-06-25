062ea2970b418a0f6dfa4cc8a29f174bb9d58b63
Alexey <alexey.anasov@yandex.by>
2024-06-25 23:11:47 +0300

    добавлен rangeSlider

--- js/controller.js
+++ js/controller.js
@@ -1,13 +1,17 @@
 import * as Model from "./model.js";
 import programs from "./view/radioPrograms.js";
 import updateResultsView from "./view/updateResultsView.js";
+
 import costInput from "./view/costInput.js";
+import costRange from "./view/costRange.js";
 
 window.onload = function () {
   const getData = Model.getData;
 
   programs(getData);
+
   costInput(getData);
+  costRange(getData);
 
   document.addEventListener("updateForm", (e) => {
     Model.setData(e.detail);
