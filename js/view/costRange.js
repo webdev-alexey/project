bc8f907591fad9cb422d5ab9c631d61e656d7a16
Alexey <alexey.anasov@yandex.by>
2024-06-26 23:17:40 +0300

    range slider обновление модели

--- js/view/costRange.js
+++ js/view/costRange.js
@@ -1,3 +1,5 @@
+import updateModel from "./../utils/updateModel.js";
+
 function init(getData) {
   const slider = document.querySelector("#slider-cost");
   const data = getData();
@@ -18,6 +20,14 @@ function init(getData) {
       thousand: " ",
       suffix: "",
     }),
+  });
+
+  slider.noUiSlider.on("slide", function () {
+    let sliderValue = slider.noUiSlider.get();
+    sliderValue = sliderValue.split(".")[0];
+    sliderValue = parseInt(String(sliderValue).replace(/ /g, ""));
+
+    updateModel(slider, { cost: sliderValue, onUpdate: "costSlider" });
   });
 }
 
