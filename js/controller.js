cd4d9f6164572e9e76e4358f64d955c138cb9179
Alexey <alexey.anasov@yandex.by>
2024-07-20 13:32:44 +0300

    форма заявки. fetch запрос

--- js/controller.js
+++ js/controller.js
@@ -77,4 +77,85 @@ window.onload = function () {
       sliderTime.noUiSlider.set(data.time);
     }
   }
+
+  const openFormBtn = document.querySelector("#openFormBtn");
+  const orderForm = document.querySelector("#orderForm");
+  const submitFormBtn = document.querySelector("#submitFormBtn");
+
+  openFormBtn.addEventListener("click", function () {
+    orderForm.classList.remove("none");
+    openFormBtn.classList.add("none");
+  });
+
+  orderForm.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    const formData = new FormData(orderForm);
+
+    submitFormBtn.setAttribute("disabled", true);
+    submitFormBtn.innerText = "Заявка отправляется...";
+
+    orderForm.querySelectorAll("input").forEach(function (input) {
+      input.setAttribute("disabled", true);
+    });
+
+    fetchData();
+
+    async function fetchData() {
+      const data = Model.getData();
+      const results = Model.getResults();
+
+      let url = checkOnUrl(document.location.href);
+
+      function checkOnUrl(url) {
+        let urlArrayDot = url.split(".");
+
+        if (urlArrayDot[urlArrayDot.length - 1] === "html") {
+          urlArrayDot.pop();
+          let newUrl = urlArrayDot.join(".");
+          let urlArraySlash = newUrl.split("/");
+          urlArraySlash.pop();
+          newUrl = urlArraySlash.join("/") + "/";
+          return newUrl;
+        }
+
+        return url;
+      }
+
+      const response = await fetch(url + "mail.php", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json;charset=utf-8",
+        },
+        body: JSON.stringify({
+          form: {
+            name: formData.get("name"),
+            email: formData.get("email"),
+            phone: formData.get("phone"),
+          },
+          data,
+          results,
+        }),
+      });
+
+      const result = await response.text();
+      console.log(result);
+
+      submitFormBtn.removeAttribute("disabled", true);
+      submitFormBtn.innerText = "Оформить заявку";
+
+      orderForm.querySelectorAll("input").forEach((input) => {
+        input.removeAttribute("disabled", true);
+      });
+
+      orderForm.reset();
+      orderForm.classList.add("none");
+
+      if (result === "SUCCESS") {
+        document.getElementById("success").classList.remove("none");
+      } else {
+        document.getElementById("error").classList.remove("none");
+      }
+    }
+  });
 };
